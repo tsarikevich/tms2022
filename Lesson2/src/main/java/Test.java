ec3bbc7ea626ea63c32597e7191880db978391ed
tsarikevich <ma.tsarikevich@gmail.com>
2022-01-17 20:44:41 +0300

    добавили лекцию 2

--- Lesson2/src/main/java/Test.java
+++ Lesson2/src/main/java/Test.java
@@ -3,5 +3,7 @@ import java.sql.SQLOutput;
 public class Test {
     public static void main(String[] args){
         System.out.println("Ghbf");
+        System.out.println("Ghbf");
+        System.out.println("1");
     }
 }
