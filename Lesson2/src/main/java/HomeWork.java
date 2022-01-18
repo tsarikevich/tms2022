697fb89152d5c33725030087ad57c4f9da72e654
tsarikevich <ma.tsarikevich@gmail.com>
2022-01-18 13:13:22 +0300

    delete

---
+++ Lesson2/src/main/java/HomeWork.java
@@ -0,0 +1,42 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        printLiterals();
+        System.out.println(sum(100, 200));
+        System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        System.out.println(max(56, 349));
+        System.out.println(calculateHypotenuse(3, 4));
+    }
+
+    private static void printLiterals() {
+        boolean lit1 = true;
+        String lit2 = "Hello";
+        char lit3 = 'V';
+        int lit4 = 0b1111;
+        int lit5 = 0564;
+        int lit6 = 235;
+        int lit7 = 0x141D2;
+        float lit8 = 2.32F;
+        double lit9 = 150000.45;
+        System.out.println(lit1);
+        System.out.println(lit2);
+        System.out.println(lit3);
+        System.out.println(lit4);
+        System.out.println(lit5);
+        System.out.println(lit6);
+        System.out.println(lit7);
+        System.out.println(lit8);
+        System.out.println(lit9);
+            }
+
+    public static int sum(int a, int b) {
+        return 0;
+    }
+
+    public static int max(int a, int b) {
+        return 0;
+    }
+
+    public static double calculateHypotenuse(int a, int b) {
+        return 0;
+    }
+}
