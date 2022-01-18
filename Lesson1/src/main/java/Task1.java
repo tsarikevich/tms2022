1d18cc92b8d56adabb041db2a8dbc3fe7f3d54ad
tsarikevich <ma.tsarikevich@gmail.com>
2022-01-18 09:30:39 +0300

    delete from branch (lesson2) program for lesson1

--- Lesson1/src/main/java/Task1.java
+++ Lesson1/src/main/java/Task1.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        int a;
         int count = 0;
         System.out.println("Введите 5 чисел");
         Scanner c = new Scanner(System.in);
         for (int b = 0; b < 5; b++) {
-            a = c.nextInt();
+            int a = c.nextInt();
             if (a > 0) {
                 count++;
             }
