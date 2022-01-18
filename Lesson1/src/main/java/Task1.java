868e880b524c48c5517448182036569eec0a8521
tsarikevich <ma.tsarikevich@gmail.com>
2022-01-18 09:20:15 +0300

    Lesson 1, Task 2

---
+++ Lesson1/src/main/java/Task1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a;
+        int count = 0;
+        System.out.println("Введите 5 чисел");
+        Scanner c = new Scanner(System.in);
+        for (int b = 0; b < 5; b++) {
+            a = c.nextInt();
+            if (a > 0) {
+                count++;
+            }
+        }
+        System.out.println("Количество положительных чисел равно" + " " + count);
+    }
+
+}
+
