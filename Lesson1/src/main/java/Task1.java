8bf03bae6d7776faeb95dc1ed0091afa2cc2cd1d
tsarikevich <ma.tsarikevich@gmail.com>
2022-01-20 09:31:11 +0300

    20.01.2022 9:20 update Lesson1

--- Lesson1/src/main/java/Task1.java
+++ Lesson1/src/main/java/Task1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        int count = 0;
+        int count=0;
         System.out.println("Введите 5 чисел");
         Scanner c = new Scanner(System.in);
         for (int b = 0; b < 5; b++) {
