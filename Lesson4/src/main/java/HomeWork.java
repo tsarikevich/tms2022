6bc5415cb7890254a1667482c828b1b6ed1045cc
tsarikevich <ma.tsarikevich@gmail.com>
2022-01-26 18:23:43 +0300

    add solving of task 1-9

--- Lesson4/src/main/java/HomeWork.java
+++ Lesson4/src/main/java/HomeWork.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWork {
@@ -28,6 +30,8 @@ public class HomeWork {
             case 6, 7:
                 System.out.println("Выходной");
                 break;
+            default:
+                System.out.println("Вы ввели неккоректную дату");
         }
 
 //2) Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
@@ -43,6 +47,7 @@ public class HomeWork {
 //        Надо вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
 //        Например, Введите число: 5
 //        "5 - это положительное число, количество цифр = 1"
+        System.out.println("Задча 3: определение свойств чисел");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
         int number = scanner.nextInt();
@@ -67,6 +72,124 @@ public class HomeWork {
 
 
 //4) Дано 2 числа, день и месяц рождения. Написать программу, которая определяет знак зодиака человека.
+        System.out.println("Задача 4: определение знака зодиака");
+        System.out.println("Введите день рождения:");
+        int day = scanner.nextInt();
+        System.out.println("Введите месяц рождения:");
+        int month = scanner.nextInt();
+        switch (month) {
+            case 1:
+                if (day <= 20 && day > 0) {
+                    System.out.println("Козерог");
+                } else if (day > 20 && day < 31) {
+                    System.out.println("Водолей");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 2:
+                if (day <= 19 && day > 0) {
+                    System.out.println("Водолей");
+                } else if (day > 19 && day < 29) {
+                    System.out.println("Рыбы");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 3:
+                if (day <= 20 && day > 0) {
+                    System.out.println("Рыбы");
+                } else if (day > 20 && day < 31) {
+                    System.out.println("Овен");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 4:
+                if (day <= 20 && day > 0) {
+                    System.out.println("Овен");
+                } else if (day > 20 && day < 30) {
+                    System.out.println("Телец");
+                } else {
+                    System.out.println("Вы ввели неккоректный день рождения");
+                }
+                break;
+            case 5:
+                if (day <= 21 && day > 0) {
+                    System.out.println("Телец");
+                } else if (day > 21 && day < 31) {
+                    System.out.println("Близнецы");
+                } else {
+                    System.out.println("Вы ввели неккоректный день рождения");
+                }
+                break;
+            case 6:
+                if (day <= 21 && day > 0) {
+                    System.out.println("Близнецы");
+                } else if (day > 21 && day < 30) {
+                    System.out.println("Рак");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 7:
+                if (day <= 22 && day > 0) {
+                    System.out.println("Рак");
+                } else if (day > 22 && day < 31) {
+                    System.out.println("Лев");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 8:
+                if (day <= 21 && day > 0) {
+                    System.out.println("Лев");
+                } else if (day > 21 && day < 31) {
+                    System.out.println("Дева");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 9:
+                if (day <= 23 && day > 0) {
+                    System.out.println("Дева");
+                } else if (day > 23 && day < 30) {
+                    System.out.println("Весы");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 10:
+                if (day <= 23 && day > 0) {
+                    System.out.println("Весы");
+                } else if (day > 23 && day < 31) {
+                    System.out.println("Скорпион");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 11:
+                if (day <= 23 && day > 0) {
+                    System.out.println("Скорпион");
+                } else if (day > 23 && day < 30) {
+                    System.out.println("Стрелец");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            case 12:
+                if (day <= 22 && day > 0) {
+                    System.out.println("Стрелец");
+                } else if (day > 22 && day < 30) {
+                    System.out.println("Козерог");
+                } else {
+                    System.out.println("Вы ввели неккоректный день");
+                }
+                break;
+            default:
+                System.out.println("Вы ввели неккоректный месяц");
+        }
+
 
 //        Овен (21 марта – 20 апреля)
 //        Телец (21 апреля – 21 мая)
@@ -80,18 +203,20 @@ public class HomeWork {
 //        Козерог (23 декабря — 20 января)
 //        Водолей (21 января — 19 февраля)
 //        Рыбы (20 февраля — 20 марта)
-        //Некоторые тесты для проверки задач. Можно также написать свои тесты.
+//        Некоторые тесты для проверки задач. Можно также написать свои тесты.
         printArray();
         System.out.println(operation(1));
         System.out.println(operation(0));
         System.out.println(calculateCountOfOddElementsInMatrix(new int[]{1, 2, 3, 4, 5, 6}));
         countDevs(103);
         countDevs(11);
+        countDevs(12);
         foobar(6);
         foobar(10);
         foobar(15);
         printPrimeNumbers();
     }
+
 
     /**
      * Необходимо прочитать с консоли значение числа типа int,
@@ -102,7 +227,23 @@ public class HomeWork {
      * далее вывести массив на консоль
      */
     private static void printArray() {
-        // тут пишем логику
+        System.out.println("Задача 5: заданный одномерный массив случайных элементов");
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Введите положительное число:");
+            number = scanner.nextInt();
+            if (number <= 0) {
+                System.out.println("Ошибка ввода");
+            }
+        } while (number <= 0);
+
+        int[] array = new int[number];
+        Random random = new Random();
+        for (int i = 0; i < number; i++) {
+            array[i] = random.nextInt();
+        }
+        System.out.println(Arrays.toString(array));
     }
 
     /**
@@ -113,8 +254,15 @@ public class HomeWork {
      * вернуть number после выполнения операций
      */
     public static int operation(int number) {
-        // тут пишем логику
-        return 0;
+        System.out.println("Задача 6: number");
+        if (number > 0) {
+            number++;
+        } else if (number < 0) {
+            number -= 2;
+        } else if (number == 0) {
+            number = 10;
+        }
+        return number;
     }
 
     /**
@@ -123,8 +271,17 @@ public class HomeWork {
      * в котором это значение распечатается на консоль.
      */
     public static int calculateCountOfOddElementsInMatrix(int[] ints) {
-        // тут пишем логику
-        return 0;
+        System.out.println("Задача 7: Количество нечетных элементов в массиве");
+        int count1 = 0;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == 0) {
+                continue;
+            }
+            if (ints[i] % 2 != 0) {
+                count1++;
+            }
+        }
+        return count1;
     }
 
     /**
@@ -137,7 +294,15 @@ public class HomeWork {
      * @param count - количество программистов
      */
     public static void countDevs(int count) {
-        // тут пишем логику
+        System.out.println("Задача 8: Количество пограммистов");
+        if (count == 1 | count % 10 == 1 ^ count == 11) {
+            System.out.println(count + " программист");
+        } else if (count > 1 & count < 5 | count % 10 > 1 & count % 10 < 5
+                && count != 12 && count != 13 && count != 14) {
+            System.out.println(count + " программиста");
+        } else {
+            System.out.println(count + " программистов");
+        }
     }
 
     /**
@@ -147,7 +312,16 @@ public class HomeWork {
      * - если остаток от деления на 3 и 5 равен нулю 0 ,то вывести "foobar" (example of number - 15)
      */
     public static void foobar(int number) {
-        // тут пишем логику
+        System.out.println("Задача 9: foo or bar or foobar");
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println("foobar");
+        } else if (number % 3 == 0) {
+            System.out.println("foo");
+        } else if (number % 5 == 0) {
+            System.out.println("bar");
+        } else {
+            System.out.println("Число не попадает по условие задачи");
+        }
     }
 
     /**
