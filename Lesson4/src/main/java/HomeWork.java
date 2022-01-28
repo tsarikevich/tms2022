35f8da42280eccdf447e30f1c46f977271b0c392
tsarikevich <ma.tsarikevich@gmail.com>
2022-01-28 13:05:26 +0300

    add edit count of programmers

--- Lesson4/src/main/java/HomeWork.java
+++ Lesson4/src/main/java/HomeWork.java
@@ -291,9 +291,10 @@ public class HomeWork {
     public static void countDevs(int count) {
         if (count % 10 == 1 && count % 100 != 11) {
             System.out.println(count + " программист");
-        } else if (count % 10 == 2 && count % 100 != 12
-                || count % 10 == 3 && count % 100 != 13
-                || count % 10 == 4 && count % 100 != 14) {
+//        } else if (count % 10 == 2 && count % 100 != 12
+//                || count % 10 == 3 && count % 100 != 13
+//                || count % 10 == 4 && count % 100 != 14) {
+        } else if (count % 10 >= 2 && count % 10 <= 4 && !(count % 100 >= 12 && count % 100 <= 14)) {
             System.out.println(count + " программиста");
         } else {
             System.out.println(count + " программистов");
