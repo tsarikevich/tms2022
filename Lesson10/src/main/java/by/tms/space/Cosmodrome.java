50b3939d00a6c5278b1c390d492fdd6e413cd333
tsarikevich <ma.tsarikevich@gmail.com>
2022-02-15 16:17:34 +0300

    add task3

---
+++ Lesson10/src/main/java/by/tms/space/Cosmodrome.java
@@ -0,0 +1,22 @@
+package by.tms.space;
+
+
+public class Cosmodrome {
+    IStart iStart;
+
+    void start(IStart iStart) {
+        if (!iStart.preLaunchSystemCheck()) {
+            System.out.println("Предстартовая проверка провалена");
+        } else {
+            System.out.println("Предстартовая проверка прошла успешно");
+            System.out.println("Обратный отсчёт для запуска начался:");
+            for (int i = 10; i >= 0; i--) {
+                System.out.print(i + "...");
+            }
+            iStart.onEngine();
+            iStart.start();
+        }
+    }
+
+
+}
