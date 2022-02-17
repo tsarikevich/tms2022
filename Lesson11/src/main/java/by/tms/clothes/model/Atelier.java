c76839e1d6489a282d3ffa377b19eb5c73a41cab
tsarikevich <ma.tsarikevich@gmail.com>
2022-02-17 20:55:15 +0300

    add corrected task3

--- Lesson11/src/main/java/by/tms/clothes/model/Atelier.java
+++ Lesson11/src/main/java/by/tms/clothes/model/Atelier.java
@@ -6,39 +6,51 @@ public class Atelier {
     private Skirt skirt;
     private Tshirt tshirt;
     private Trousers trousers;
+    private Clothes thing;//для вывода информации о размере
+    private SizeOfClothes sizeOfClothes;
 
 
     public void dressMan(Clothes[] clothes) {
         for (Clothes thing : clothes) {
+            this.thing = thing;
             if (thing instanceof Tie) {
                 tie = (Tie) thing;
                 tie.dressMan();
+                System.out.println(tie + " " + this.thing.getSize().getDescription());
             } else if (thing instanceof Trousers) {
                 trousers = (Trousers) thing;
                 trousers.dressMan();
+                System.out.println(trousers + " " + this.thing.getSize().getDescription());
             } else if (thing instanceof Tshirt) {
                 tshirt = (Tshirt) thing;
                 tshirt.dressMan();
+                System.out.println(tshirt + " " + this.thing.getSize().getDescription());
             }
         }
     }
-
 
     public void dressWomen(Clothes[] clothes) {
         for (Clothes thing : clothes) {
+            this.thing = thing;
             if (thing instanceof Skirt) {
                 skirt = (Skirt) thing;
                 skirt.dressWomen();
-                System.out.println(skirt);
+                System.out.println(skirt + " " + this.thing.getSize().getDescription());
             } else if (thing instanceof Trousers) {
                 trousers = (Trousers) thing;
                 trousers.dressWomen();
+                System.out.println(trousers + " " + this.thing.getSize().getDescription());
             } else if (thing instanceof Tshirt) {
                 tshirt = (Tshirt) thing;
                 tshirt.dressWomen();
+                sizeOfClothes = tshirt.getSize();
+                System.out.println(sizeOfClothes.getDescription());
+                System.out.println(tshirt + " " + this.thing.getSize().getDescription());
             }
         }
 
     }
+
+
 }
 
