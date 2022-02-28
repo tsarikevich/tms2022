c0583fae5278d6c1331b7d718a39e4766a721347
tsarikevich <ma.tsarikevich@gmail.com>
2022-02-28 11:40:13 +0300

    add corrected tasks (3,4,6) of Lesson13

--- Lesson13/src/main/java/com/tms/task3/MainTask3.java
+++ Lesson13/src/main/java/com/tms/task3/MainTask3.java
@@ -1,13 +1,14 @@
 package com.tms.task3;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class MainTask3 {
     public static void main(String[] args) {
         /** 3) В массиве находятся слова. Вывести на экран слова палиндромы
          *(т.е которые читаются справа на лево и слева на право одинаково, например: заказ, казак, дед...)*/
-        StringBuilder[] words = new StringBuilder[]{new StringBuilder("доход"), new StringBuilder("машина"), new StringBuilder("репер"), new StringBuilder("карета"), new StringBuilder("довод")};
-        for (StringBuilder word : words) {
-            StringBuilder tempWord = new StringBuilder(word).reverse();
-            if (word.toString().equals(tempWord.toString())) {
+        String[] words = {"доход", "машина", "репер", "карета", "довод"};
+        for (String word : words) {
+            if (word.equalsIgnoreCase(StringUtils.reverse(word))) {
                 System.out.println(word);
             }
         }
