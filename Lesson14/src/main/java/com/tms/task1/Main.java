cfed6c8933dcfba67651d3ba2647fbb6e2184e32
tsarikevich <ma.tsarikevich@gmail.com>
2022-03-03 17:20:57 +0300

    add tasks of lesson14

--- Lesson14/src/main/java/com/tms/task1/Main.java
+++ Lesson14/src/main/java/com/tms/task1/Main.java
@@ -1,3 +1,5 @@
+package com.tms.task1;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
@@ -24,13 +26,14 @@ import java.io.*;
  */
 
 public class Main {
-    public static final String INPUT_FILE = "Lesson14/src/main/java/hw1/input.txt";
-    public static final String OUTPUT_FILE = "Lesson14/src/main/java/hw1/output.txt";
+    public static final String INPUT_FILE = "Lesson14/src/main/java/com/tms/task1/hw1/input.txt";
+    public static final String OUTPUT_FILE = "Lesson14/src/main/java/com/tms/task1/hw1/output.txt";
 
     public static void main(String[] args) {
         /**
          * 1)В исходном файле hw1/input.txt находятся слова, каждое слово на новой строке.
          * После запуска программы должен создать файл output.txt, который будет содержать в себе только палиндромы.*/
+
         try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE));
              BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE))) {
             String line;
@@ -44,18 +47,6 @@ public class Main {
             e.printStackTrace();
         }
     }
-
-    /** 2)Текстовый файл hw2/input.txt содержит текст.
-     * После запуска программы в другой файл должны записаться только те предложения, в которых от 3-х до 5-ти слов.
-     * Если в предложении присутствует слово-палиндром, то не имеет значения какое кол-во слов в предложении, оно попадает в output.txt файл.
-     * Пишем все в ООП стиле. Создаем класс TextFormatter
-     * в котором два метода:
-     * 1. Метод принимает строку и возвращает кол-во слов в строке.
-     * 2. Метод принимает строку и проверяет есть ли в строке слово-палиндром. Если есть возвращает true, если нет false
-     * В main считываем файл.
-     * Разбиваем текст на предложения. Используя методы класса TextFormatter определяем подходит ли нам предложение.
-     * Если подходит добавляем его в output.txt файл */
-
 
 }
 
---
+++ Lesson14/src/main/java/com/tms/task2/TextFormatter.java
@@ -0,0 +1,45 @@
+package com.tms.task2;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+
+public class TextFormatter {
+
+    public void printSentence(String string) {
+        if (!StringUtils.isEmpty(string)) {
+            String[] sentences = string.split("[.!?]");
+            for (String sentence : sentences) {
+                if (3 <= getNumberWords(sentence) && getNumberWords(sentence) <= 5) {
+                    System.out.println(sentence.trim());
+                } else if (isPolidrome(sentence)) {
+                    System.out.println(sentence.trim());
+                }
+            }
+        } else {
+            System.out.println("Вы ввели пустую строку или null");
+        }
+    }
+
+    private Boolean isPolidrome(String string) {
+        String[] words = string.trim().split(" ");
+        for (String word : words) {
+            if (NumberUtils.isDigits(word) || word.length() < 2) {
+                continue;
+            }
+            if (word.equalsIgnoreCase(StringUtils.reverse(word))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getNumberWords(String string) {
+        return string.trim().split(" ").length;
+    }
+}
+
+
+
+
+
