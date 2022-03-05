9cfa78a8a533eee4d10019c614db44d1c96b8893
tsarikevich <ma.tsarikevich@gmail.com>
2022-03-05 22:29:46 +0300

    add tasks of Lesson14

--- Lesson14/src/main/java/com/tms/task5/Main.java
+++ Lesson14/src/main/java/com/tms/task5/Main.java
@@ -1,5 +1,11 @@
 package com.tms.task5;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tms.task5.hw5.Car;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * 5) Задача со *
  * Скопировать объект Car из пакета hw5, добавить библиотеки
@@ -10,7 +16,9 @@ package com.tms.task5;
  * Преобразовать прочитанные данные в объект hw5.Car (название полей редактировать нельзя, добавлять поля также нельзя) и вывести на консоль объект через System.out.println();
  */
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Car car = mapper.readValue(new File("Lesson14/src/main/java/com/tms/task5/hw5/car.json"), Car.class);
+        System.out.println(car);
     }
 }
