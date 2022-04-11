81171e0fa7660f39c30179709fec97ba4786eabf
tsarikevich <ma.tsarikevich@gmail.com>
2022-04-11 22:40:39 +0300

    update init2.sq

--- Lesson20_21/src/main/resources/init2.sql
+++ Lesson20_21/src/main/resources/init2.sql
@@ -7,45 +7,48 @@ CREATE SCHEMA IF NOT EXISTS students_db;
 --------------------------------------------------------
 --  DDL for Table students_db
 --------------------------------------------------------
+DROP TABLE IF EXISTS students_db.studentsCity;
+CREATE TABLE IF NOT EXISTS students_db.studentsCity
+(
+    id   INT          NOT NULL PRIMARY KEY,
+    city VARCHAR(100) NOT NULL
+
+);
+--------------------------------------------------------
 DROP TABLE IF EXISTS students_db.students;
 CREATE TABLE IF NOT EXISTS students_db.students
 (
     id      INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name    VARCHAR(45)  NOT NULL,
     surname VARCHAR(100) NOT NULL,
-    course  INT          NOT NULL
-);
---------------------------------------------------------
-DROP TABLE IF EXISTS students_db.studentsCity;
-CREATE TABLE IF NOT EXISTS students_db.studentsCity
-(
-    idCity INT          NOT NULL UNIQUE,
-    city   VARCHAR(100) NOT NULL,
-    FOREIGN KEY (idCity) references students_db.students (id)
+    course  INT          NOT NULL,
+    city_id INT          NOT NULL,
+    FOREIGN KEY (city_id) references students_db.studentsCity (id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
-
 );
+
+
 --------------------------------------------------------
 --  DML for Table students_db
 --------------------------------------------------------
-INSERT INTO students_db.students(name, surname, course)
-VALUES ('Alex', 'Surkov', 1);
-INSERT INTO students_db.students(name, surname, course)
-VALUES ('Anna', 'Borodich', 2);
-INSERT INTO students_db.students(name, surname, course)
-VALUES ('Sergey', 'Kryvnitskiy', 3);
-INSERT INTO students_db.students(name, surname, course)
-VALUES ('Alina', 'Kedich', 4);
-INSERT INTO students_db.students(name, surname, course)
-VALUES ('Petr', 'Medvedzev', 5);
---------------------------------------------------------
-INSERT INTO students_db.studentsCity(idCity, city)
+INSERT INTO students_db.studentsCity(id, city)
 VALUES (1, 'MINSK');
-INSERT INTO students_db.studentsCity(idCity, city)
+INSERT INTO students_db.studentsCity(id, city)
 VALUES (2, 'GOMEL');
-INSERT INTO students_db.studentsCity(idCity, city)
+INSERT INTO students_db.studentsCity(id, city)
 VALUES (3, 'MOGILEV');
-INSERT INTO students_db.studentsCity(idCity, city)
+INSERT INTO students_db.studentsCity(id, city)
 VALUES (4, 'GRODNO');
+
+INSERT INTO students_db.students(name, surname, course, city_id)
+VALUES ('Alex', 'Surkov', 1, 1);
+INSERT INTO students_db.students(name, surname, course, city_id)
+VALUES ('Anna', 'Borodich', 2, 2);
+INSERT INTO students_db.students(name, surname, course, city_id)
+VALUES ('Sergey', 'Kryvnitskiy', 3, 3);
+INSERT INTO students_db.students(name, surname, course, city_id)
+VALUES ('Alina', 'Kedich', 4, 4);
+--------------------------------------------------------
+
 
