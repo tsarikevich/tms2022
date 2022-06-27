ed1c4ff25372856606164e2be4142ee61f37f3e4
tsarikevich <ma.tsarikevich@gmail.com>
2022-06-27 15:39:28 +0300

    update file LogAspect.java (Lesson38_40)

--- Lesson38_40/src/main/java/by/teachmeskills/eshop/aop/LogAspect.java
+++ Lesson38_40/src/main/java/by/teachmeskills/eshop/aop/LogAspect.java
@@ -1,14 +1,22 @@
 package by.teachmeskills.eshop.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Component
 @Aspect
 public class LogAspect {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Pointcut("execution(* by.teachmeskills.eshop.services.impl.UserServiceImpl.*(..))")
     private void userMethodExecuting() {
     }
@@ -17,17 +25,29 @@ public class LogAspect {
     private void productMethodExecuting() {
     }
 
-    @Before("userMethodExecuting()&&productMethodExecuting()")
-    public void getInfo(JoinPoint joinPoint) {
-        System.out.println(joinPoint.getSignature().getName() + " method is calling");
-        System.out.println(joinPoint.getSignature() + " method is calling");
-        System.out.println("******************************************");
-        System.out.println("******************************************");
-        System.out.println("******************************************");
-        System.out.println("******************************************");
-        System.out.println("******************************************");
-        System.out.println("******************************************");
-        System.out.println("******************************************");
-        System.out.println("******************************************");
+    @Before("userMethodExecuting()")
+    public void getUserInfo(JoinPoint joinPoint) {
+        String args = Arrays.stream(joinPoint.getArgs())
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        logger.info("before " + joinPoint + ", args=[" + args + "]");
+    }
+
+    @Before("productMethodExecuting()")
+    public void getProductInfo(JoinPoint joinPoint) {
+        String args = Arrays.stream(joinPoint.getArgs())
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        logger.info("before " + joinPoint + ", args=[" + args + "]");
+    }
+
+    @After("userMethodExecuting()")
+    public void afterCallUserService(JoinPoint jp) {
+        logger.info("after " + jp.toString());
+    }
+
+    @After("productMethodExecuting()")
+    public void afterCallProductService(JoinPoint jp) {
+        logger.info("after " + jp.toString());
     }
 }
